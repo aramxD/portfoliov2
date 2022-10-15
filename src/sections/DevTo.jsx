f0c32b293d04ef0bfc14f4e13d00f9ba61782ae5
julio santacruz <aram.jsh1@gmail.com>
2022-10-14 23:46:32 -0700

    devto api collect data

--- src/sections/DevTo.jsx
+++ src/sections/DevTo.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
-import styled from 'styled-components'
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 
+const DevTo = ({ className }) => {
+  const [userData, setUserData] = useState([]);
 
+  useEffect(() => {
+    async function fetchDevTo() {
+      const response = await fetch(
+        "https://dev.to/api/articles?username=julio_santacruz"
+      );
+      const articles = await response.json();
+      //console.log(articles);
+      setUserData(articles);
+    }
 
+    fetchDevTo();
+  }, []);
+  console.log(userData);
+  return <section className={className}>soy el devto</section>;
+};
 
-const DevTo =({className})=>{
-
-    return(
-        <section className={className}>
-            soy el devto
-        </section>
-    )
-}
-
-
-export default styled(DevTo)`
-    
-
-`
+export default styled(DevTo)``;
